dc7ccc30d8643c36cd5541d65dd98e9094c245f3
dgsakelliadou <88878812+dgsakelliadou@users.noreply.github.com>
2021-08-14 03:37:48 -0400

    Add files via upload

---
+++ alexheimersnet.py
@@ -0,0 +1,198 @@
+import os
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras.utils import plot_model
+import tqdm
+import matplotlib.pyplot as plt
+import PIL
+
+try:
+    tpu = tf.distribute.cluster_resolver.TPUClusterResolver()
+    print('Device:', tpu.master())
+    tf.config.experimental_connect_to_cluster(tpu)
+    tf.tpu.experimental.initialize_tpu_system(tpu)
+    strategy = tf.distribute.experimental.TPUStrategy(tpu)
+except:
+    strategy = tf.distribute.get_strategy()
+print('Number of replicas:', strategy.num_replicas_in_sync)
+    
+print(tf.__version__)
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+BATCH_SIZE = 16 * strategy.num_replicas_in_sync
+IMAGE_SIZE = [176, 208]
+EPOCHS = 20
+
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    "/Users/danaesakelliadou/Alzheimer_s/train",
+    ##needs edit
+    validation_split=0.2,
+    subset="training",
+    seed=1337,
+    image_size=IMAGE_SIZE,
+    batch_size=BATCH_SIZE,
+)
+
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    "/Users/danaesakelliadou/Alzheimer_s/train",
+    ##needs edit
+validation_split=0.2,
+subset="validation",
+seed=1337,
+image_size=IMAGE_SIZE,
+batch_size=BATCH_SIZE,
+)
+
+class_names = ['MildDementia', 'ModerateDementia', 'NonDementia', 'VeryMildDementia']
+train_ds.class_names = class_names
+val_ds.class_names = class_names
+
+NUM_CLASSES = len(class_names)
+
+
+##data visualization
+plt.figure(figsize=(10, 10))
+for images, labels in train_ds.take(1):
+  for i in range(9):
+    ax = plt.subplot(3, 3, i + 1)
+    plt.imshow(images[i].numpy().astype("uint8"))
+    plt.title(train_ds.class_names[labels[i]])
+    plt.axis("off")
+
+##one hot encoder - because we are dealing with non continuous data
+def one_hot_label(image, label):
+    label = tf.one_hot(label, NUM_CLASSES)
+    return image, label
+
+train_ds = train_ds.map(one_hot_label, num_parallel_calls=AUTOTUNE)
+val_ds = val_ds.map(one_hot_label, num_parallel_calls=AUTOTUNE)
+
+##makes images more calling images more efficient - but how?
+train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+##classification evaluation
+##ROC AUC is used - 0,0.5 - cant predict, 0.5 - random, >1 good predictive ability
+##we have an imbalanced dataset, a set with unequal distribution of classes so we wont use accuracy
+
+NUM_IMAGES = []
+
+for label in class_names:
+    dir_name = "/Users/danaesakelliadou/Alzheimer_s/train/" + label[:-2] + 'ed'
+    NUM_IMAGES.append(len([name for name in os.listdir(dir_name)]))
+    
+NUM_IMAGES
+
+##building the model with tf.keras - 3x3 filters , relu as activation, padding is same-
+## same means the output image with have the same dimensions as the input
+## i.e p = (f-1)/2 because then n+2p -f + 1 = n
+## batch normalization, dropout and maxpooling is used
+
+# convolution block
+def build_model():
+    model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(filters=96, kernel_size=(11,11), strides=(4,4), activation='relu', input_shape=(176, 208,3)),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
+    tf.keras.layers.Conv2D(filters=256, kernel_size=(5,5), strides=(1,1), activation='relu', padding="same"),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
+    tf.keras.layers.Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(4096, activation='relu'),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(4096, activation='relu'),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(NUM_CLASSES, activation='softmax')
+])
+
+
+    
+    return model
+
+##adam optimization and loss by CCE
+
+with strategy.scope():
+    model = build_model()
+
+    METRICS = [tf.keras.metrics.AUC(name='auc')]
+    
+    model.compile(
+        optimizer='adam',
+        loss=tf.losses.CategoricalCrossentropy(),
+        metrics=METRICS
+    )
+    tf.keras.utils.plot_model(model, to_file='modelalex.png', show_shapes=True)
+    
+
+##SECTION B: the training of the model - adjust learning rate
+##stopping the model early to prevent overfitting
+
+
+def exponential_decay(lr0, s):
+    def exponential_decay_fn(epoch):
+        return lr0 * 0.1 **(epoch / s)
+    return exponential_decay_fn
+
+exponential_decay_fn = exponential_decay(0.01, 20)
+
+lr_scheduler = tf.keras.callbacks.LearningRateScheduler(exponential_decay_fn)
+
+##why are we calling back h5
+
+checkpoint_cb = tf.keras.callbacks.ModelCheckpoint("alzheimer_model.h5",
+                                                    save_best_only=True)
+
+early_stopping_cb = tf.keras.callbacks.EarlyStopping(patience=10,
+                                                     restore_best_weights=True)
+##model fitting
+
+history = model.fit(
+    train_ds,
+    validation_data=val_ds,
+    callbacks=[checkpoint_cb, early_stopping_cb, lr_scheduler],
+    epochs=EPOCHS
+)
+
+## metric/evaluation graphs
+
+
+fig, ax = plt.subplots(1, 2, figsize=(20, 3))
+ax = ax.ravel()
+
+for i, met in enumerate(['auc', 'loss']):
+    ax[i].plot(history.history[met])
+    ax[i].plot(history.history['val_' + met])
+    ax[i].set_title('Model {}'.format(met))
+    ax[i].set_xlabel('epochs')
+    ax[i].set_ylabel(met)
+    ax[i].legend(['train', 'val'])
+
+
+## evaluation on testing dataset - need to fix directory
+
+test_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    "/Users/danaesakelliadou/Alzheimer_s/test",
+    image_size=IMAGE_SIZE,
+    batch_size=BATCH_SIZE,
+)
+
+test_ds = test_ds.map(one_hot_label, num_parallel_calls=AUTOTUNE)
+test_ds = test_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+_ = model.evaluate(test_ds)
+  
+
+
+
+
+
+
+
